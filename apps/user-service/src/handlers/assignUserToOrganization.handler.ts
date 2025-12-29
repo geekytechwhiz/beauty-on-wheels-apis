0437153e684efd7cb05bf2f06bafb999ef96f6b9
prasanthms <168095508+prasnthms@users.noreply.github.com>
2025-12-29 18:28:03 +0530

    Queue added

--- apps/user-service/src/handlers/assignUserToOrganization.handler.ts
+++
@@ -1,6 +0,0 @@
-import { APIGatewayProxyHandler, Context } from 'aws-lambda';
-import { assignUserToOrganization } from './httpHandler';
-
-export const main: APIGatewayProxyHandler = async (event, context: Context) => {
-  return assignUserToOrganization(event, context);
-};

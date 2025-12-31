20ec285c58d83ce7cad98b3038b49024ef97162e
Arpan Garg <arpan.garg@firminiq.com>
2025-12-31 13:35:44 +0530

    TLH-10023-New

--- apps/user-service/src/services/user.service.ts
+++ apps/user-service/src/services/user.service.ts
@@ -26,7 +26,6 @@ export class UserService {
       data.userID = data.code || ulid();
     }
     const logger = createChildLogger(baseLogger, { correlationId, userId: data.userID, organizationID, invitedBy });
-    logger.info({ event: 'service_createUser_start' });
 
     try {
       if (!organizationID) throw new Error('organizationID is required');

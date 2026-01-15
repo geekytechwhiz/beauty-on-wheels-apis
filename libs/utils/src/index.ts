dc6a5f043a0df5337aebcf0477bf1f61498ab01c
Arpan Garg <arpan.garg@firminiq.com>
2026-01-15 15:40:43 +0530

    feature/TLH-10060

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,6 +1,5 @@
 export * from './constants/constants';
-export * from './configs/db.config';
-export * from './configs/db.config';
+export { ddbClient, ddbDocClient } from './configs/db.config';
 export * from './helper/stringUtils';
 export * from './helper/objectUtils';
 export * from './helper/headerUtils';

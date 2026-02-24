fa10d1d4b5b8c3669ebf16748a2d56754cc9bfe0
prasanthms <168095508+prasnthms@users.noreply.github.com>
2026-02-24 16:31:27 +0530

    Script updated

---
+++ apps/organization-service/src/utils/mitadata/setup-script/script.js
@@ -0,0 +1,19 @@
+const { exec } = require('child_process');
+const path = require('path');
+
+const scriptPath = path.join(__dirname, 'setup.sh');
+
+export const setupScript = async () => {
+  try {
+    exec(`bash "${scriptPath}"`, (error, stdout, stderr) => {
+      if (error) {
+        console.error('Script failed:', error);
+        console.error(stderr);
+        return;
+      }
+      console.log('Script output:', stdout);
+    });
+  } catch (error) {
+    console.error('Script failed:', error);
+  }
+};

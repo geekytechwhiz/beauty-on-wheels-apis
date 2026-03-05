4368b4ec2454c297cd07db337925bc05ea17a26f
prasanthms <168095508+prasnthms@users.noreply.github.com>
2026-03-05 23:14:33 +0530

    Build issue

--- services/sso-integration/src/services/launch.service.ts
+++ services/sso-integration/src/services/launch.service.ts
@@ -99,7 +99,7 @@ export class LaunchService extends BaseService {
       }
 
       const mappedAppointments = this.truTechAdapter.mapAppointments(
-        appointmentsResponse,
+        appointmentsResponse.appointments || [],
       );
 
       return {

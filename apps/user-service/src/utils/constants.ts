69c61fe06cb7e6ade7d5c51d60f59538c0ade980
prasanthms <168095508+prasnthms@users.noreply.github.com>
2026-03-05 17:45:33 +0530

    Welcom message

--- apps/user-service/src/utils/constants.ts
+++ apps/user-service/src/utils/constants.ts
@@ -11,6 +11,7 @@ export const PATH_ASSIGNED_PACKAGES = '/user/{userId}/organization/{organization
 export const PATH_V2_USER_LIST = '/v2/user/list';
 // export const WELCOME_MESSAGE = 'Welcome to {{ORG_NAME}}. Your account has been created. Thank you for MyVitalRx.';
 export const WEB_DNS_URL = process.env.WEB_URL; 
+export const WELCOME_DLT_CONTENT_ID = "1207176414422859894";
 export const WELCOME_MESSAGE = 'Welcome to {{ORG_NAME}}! To get started and access our services, please sign in using this link: {{PORTAL_LINK}}. Powered by MyVitalRx';
 // Email templates and messages
 

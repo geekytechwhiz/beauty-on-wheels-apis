392c968f405f22e68b390a8e8950808b2a724813
prasanthms <168095508+prasnthms@users.noreply.github.com>
2026-03-11 13:54:44 +0530

    Sechudler modified

--- services/sso-integration/src/services/appointment-sync.service.ts
+++ services/sso-integration/src/services/appointment-sync.service.ts
@@ -743,7 +743,7 @@ export class AppointmentSyncService extends BaseService {
           appointment,
           doctorUser,
           patientUser,
-          orgAddonId
+          orgAddonId || ''
         );
 
       logger.info({

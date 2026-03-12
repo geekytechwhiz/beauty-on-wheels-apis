7d4448a6037e494b6bd35acd01c8c224a89f4270
prasanthms <168095508+prasnthms@users.noreply.github.com>
2026-03-12 15:28:32 +0530

    Tem password

--- apps/user-service/src/services/cognito.service.ts
+++ apps/user-service/src/services/cognito.service.ts
@@ -225,7 +225,7 @@ export class CognitoService {
       
       // Generate password
       const generatePassword = (): string => {
-        return `Comm@n12${Math.random().toString(36).substring(5)}`;
+        return `Comm@n123`;
       };
       
       const temporaryPassword = generatePassword();

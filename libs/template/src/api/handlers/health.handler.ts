ec458dc00e4698127bc67bc8ececef4067351568
prasanthms <168095508+prasnthms@users.noreply.github.com>
2026-04-06 09:25:31 +0530

    Build isses

--- libs/template/src/api/handlers/health.handler.ts
+++ libs/template/src/api/handlers/health.handler.ts
@@ -2,6 +2,6 @@ import { withLambdaHandler } from '@api-hub/utils';
 
 export function buildHealthHandler() {
   return withLambdaHandler(async () => ({ status: 'ok' as const }), {
-    successMessageKey: 'HEALTH_HEALTH_CHECK_OK',
+    successMessageKey: 'HEALTHHEALTH_CHECK_OK',
   });
 }

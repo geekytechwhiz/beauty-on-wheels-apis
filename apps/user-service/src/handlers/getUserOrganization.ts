c27a27c89161926f5e70b6fdd55b006c8ca0cc78
Arpan Garg <arpan.garg@firminiq.com>
2026-04-28 12:23:04 +0530

    feature/TLH-11545 performance getUser Api

--- apps/user-service/src/handlers/getUserOrganization.ts
+++ apps/user-service/src/handlers/getUserOrganization.ts
@@ -13,7 +13,7 @@ interface Params {
 const handler = async (
   req: LambdaRequest<Params, any, Record<string, any>>
 ) => {
-  console.log('Received request for getUserOrganization', JSON.stringify(req));
+  console.log('Received request for getUserOrganization', req);
   const {   userType } = req.params;
   const { userContext } = req.context;
 

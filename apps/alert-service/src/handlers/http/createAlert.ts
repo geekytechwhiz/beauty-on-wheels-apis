8a474c7e5616e43817cac4672fb72ca6b57c2dfa
rizwan <rizwan.ar@firminiq.com>
2026-05-08 11:53:31 +0530

    corrected the sla

--- apps/alert-service/src/handlers/http/createAlert.ts
+++ apps/alert-service/src/handlers/http/createAlert.ts
@@ -1,8 +1,8 @@
-import { withApiHandler, type LambdaRequest } from '@api-hub/middleware';
-
+import { withApiHandler } from '@api-hub/middleware';
 import { getAlertHttpController } from '../../controllers/alert-http.controller';
 import { createAlertHttpBodySchema } from '../../validators/alert.schemas';
 import { validateCreateAlertRequest } from '../../validators/request.validators';
+import { LambdaRequest } from '@api-hub/utils';
 
 const c = getAlertHttpController();
 

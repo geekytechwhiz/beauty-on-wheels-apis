269c6142f228c30041c80505be692be241bf1c6b
prasanthms <168095508+prasnthms@users.noreply.github.com>
2026-05-28 04:19:37 +0530

    CORS enabled

--- apps/alert-service/src/handlers/http/listFiles.ts
+++ apps/alert-service/src/handlers/http/listFiles.ts
@@ -1,13 +1,22 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { s3Service } from '../../controllers/file-upload.controller';
 
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': '*',
+  'Access-Control-Allow-Methods': 'GET,OPTIONS',
+};
+
 export const handler = async (event: APIGatewayProxyEvent) => {
   try {
+  
+
     const folder = event.queryStringParameters?.folder;
 
     if (!folder) {
       return {
         statusCode: 400,
+        headers: corsHeaders,
         body: JSON.stringify({
           message: 'folder query parameter is required',
         }),
@@ -18,6 +27,7 @@ export const handler = async (event: APIGatewayProxyEvent) => {
 
     return {
       statusCode: 200,
+      headers: corsHeaders,
       body: JSON.stringify({ files }),
     };
   } catch (error) {
@@ -25,6 +35,7 @@ export const handler = async (event: APIGatewayProxyEvent) => {
 
     return {
       statusCode: 500,
+      headers: corsHeaders,
       body: JSON.stringify({
         message: 'Unable to list files',
       }),

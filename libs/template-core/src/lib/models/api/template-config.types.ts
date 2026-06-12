d636948558fc8c648b52b73c7e2bfb09e0f83cf9
Mehul <mehul.chhotala@firminiq.com>
2026-06-12 17:47:36 +0530

    change in reponse with set outside of document

--- libs/template-core/src/lib/models/api/template-config.types.ts
+++ libs/template-core/src/lib/models/api/template-config.types.ts
@@ -1,5 +1,10 @@
+export const TEMPLATE_CONFIG_TYPES = ['TEMPLATE', 'ORG'] as const;
+export type TemplateConfigType = (typeof TEMPLATE_CONFIG_TYPES)[number];
+
 export type TemplateConfigRecord = {
   configId: string;
+  configType?: TemplateConfigType;
+  templateType?: string;
   document: Record<string, unknown>;
 };
 
